3798cc77298a0e10a90a49901bbf11df214211df
John <john.groenhof@gmail.com>
2018-01-11 22:06:24 -0700

    new framework build settings

---
+++ iOSDistanceSDK/iOSDistanceSDK.framework/Headers/iOSDistanceSDK.h
@@ -0,0 +1,19 @@
+//
+//  iOSDistanceSDK.h
+//  iOSDistanceSDK
+//
+//  Created by John on 12/20/17.
+//  Copyright © 2017 KS Technologies, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOSDistanceSDK.
+FOUNDATION_EXPORT double iOSDistanceSDKVersionNumber;
+
+//! Project version string for iOSDistanceSDK.
+FOUNDATION_EXPORT const unsigned char iOSDistanceSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSDistanceSDK/PublicHeader.h>
+
+
